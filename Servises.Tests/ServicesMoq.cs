487ad67435045f8f1aa7cf317b4e06ef53e27a0d
dsad <Admin@DESKTOP-2R5E6P7>
2024-11-25 21:09:27 +0300

    klkk

---
+++ Servises.Tests/ServicesMoq.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+
+namespace Servises.Tests
+{
+    [TestFixture]
+    public class ServicesMoq
+    {
+        Mock<MySqlConnection> _connnection;
+        Mock<MySqlCommand> _command;
+        Mock<MySqlDataAdapter> _adapter;
+        Mock<Initialisation> _services;
+
+        [SetUp]
+        public void SetUp() {
+            _services = new Mock<Initialisation>();
+            _connnection = new Mock<MySqlConnection>(string.Empty);
+            _command = new Mock<MySqlCommand>(string.Empty, _connnection.Object);
+            _adapter = new Mock<MySqlDataAdapter>(_command.Object);
+
+            _connnection.Setup(s => s.Open());
+            _command.Setup(c => c.ExecuteReader()).Returns(new Mock<MySqlDataReader>().Object);
+        }
+    }
+}
